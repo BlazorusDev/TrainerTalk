0f56497b2806c43c83ea8b7c9f6d134a06ef7a8b
Mohit-Mehra <info.mohit728@gmail.com>
2025-05-11 20:22:55 +0530

    Fix lightningcss build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,17 @@
 import { Toaster } from "sonner";
 import type { Metadata } from "next";
-import { Mona_Sans } from "next/font/google";
+import { Inter } from "next/font/google";
 
 import "./globals.css";
 
-const monaSans = Mona_Sans({
+const monaSans = Inter({
   variable: "--font-mona-sans",
   subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "TrainerTalk - AI Trainer",
-  description:
-    "TrainerTalk is a real-time AI voice interview platform that helps users practice and improve their interview skills. It offers realistic mock interviews with instant feedback—perfect for leveling up your portfolio and confidence. Whether you're a job seeker or a hiring manager, TrainerTalk is your go-to tool for mastering interviews.",
+  title: "PrepWise",
+  description: "An AI-powered platform for preparing for mock interviews",
 };
 
 export default function RootLayout({
@@ -24,6 +23,7 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <body className={`${monaSans.className} antialiased pattern`}>
         {children}
+
         <Toaster />
       </body>
     </html>
